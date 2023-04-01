b93565ffbbc3a239e83f3a35da054b34dcb57470
bashayer770 <beshow95@gmail.com>
2023-04-01 23:57:12 +0300

    Basic styling

--- src/Components/BeenPage.js
+++ src/Components/BeenPage.js
@@ -2,17 +2,57 @@ import React from "react";
 import WorldMap from "../svg/worldMap";
 import { countries } from "./CountryData";
 const BeenPage = () => {
+  const been = [countries.Argentina, countries.Italy];
   return (
-    <div>
-      <WorldMap
-        className={"map"}
-        been={[
-          countries.Kuwait,
-          countries.Afghanistan,
-          countries["United Arab Emirates (the)"],
-        ]}
-      />
-    </div>
+    <>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+        }}
+      >
+        <div
+          style={{
+            width: "20%",
+            height: "100%",
+            backgroundColor: "grey",
+          }}
+        ></div>
+
+        <div
+          style={{
+            width: "80%",
+            height: "100%",
+            backgroundColor: "black",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <div
+            style={{
+              height: "10%",
+              width: "100%",
+              backgroundColor: "red",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            BEEN
+          </div>
+          <div
+            style={{
+              height: "100%",
+              overflow: "scroll",
+              marginTop: "1%",
+            }}
+          >
+            <WorldMap className={"map"} been={been} width="2000" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
