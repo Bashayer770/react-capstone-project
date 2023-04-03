adb5dc241b5511ef9d4dad6d36bce679456c8265
Bashayer770 <126584700+Bashayer770@users.noreply.github.com>
2023-04-03 17:43:04 +0300

    map adjusted

--- src/Components/BeenPage.js
+++ src/Components/BeenPage.js
@@ -3,25 +3,30 @@ import WorldMap from "../svg/worldMap";
 import { countries } from "./CountryData";
 import slogo from "../Assests/slogo.png";
 import Pluscircle from "../svg/pluscircle";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 const BeenPage = () => {
   const been = [countries.Argentina, countries.Italy];
   return (
     <>
-      <div className="mapContainer">
-        <div className="BEEN">
-          B E E N
-          <div className="BEEN2">
-            <div className="addCountryButton">
-              <Pluscircle />
+      <TransformWrapper>
+        <TransformComponent>
+          <div className="mapContainer">
+            <div className="BEEN">
+              B E E N
+              <div className="BEEN2">
+                <div className="addCountryButton">
+                  <Pluscircle />
+                </div>
+              </div>
+            </div>
+            <div className="beenUnderLine"></div>
+
+            <div className="scrollBar">
+              <WorldMap className={"map"} been={been} width="2000" />
             </div>
           </div>
-        </div>
-        <div className="beenUnderLine"></div>
-
-        <div className="scrollBar">
-          <WorldMap className={"map"} been={been} width="2000" />
-        </div>
-      </div>
+        </TransformComponent>
+      </TransformWrapper>
     </>
   );
 };
--- src/Components/PlanningPage.js
+++ src/Components/PlanningPage.js
@@ -3,26 +3,31 @@ import WorldMap from "../svg/worldMap";
 import { countries } from "./CountryData";
 import slogo from "../Assests/slogo.png";
 import Pluscircle from "../svg/pluscircle";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const PlanningPage = () => {
   const Planning = [countries.Canada, countries.Spain];
   return (
     <>
-      <div className="mapContainer">
-        <div className="PLAN">
-          P L A N N I N G
-          <div className="PLAN2">
-            <div className="addCountryButton2">
-              <Pluscircle />
+      <TransformWrapper>
+        <TransformComponent>
+          <div className="mapContainer">
+            <div className="PLAN">
+              P L A N N I N G
+              <div className="PLAN2">
+                <div className="addCountryButton2">
+                  <Pluscircle />
+                </div>
+              </div>
+            </div>
+            <div className="beenUnderLine2"></div>
+
+            <div className="scrollBar">
+              <WorldMap className={"map"} been={Planning} width="2000" />
             </div>
           </div>
-        </div>
-        <div className="beenUnderLine2"></div>
-
-        <div className="scrollBar">
-          <WorldMap className={"map"} been={Planning} width="2000" />
-        </div>
-      </div>
+        </TransformComponent>
+      </TransformWrapper>
     </>
   );
 };
