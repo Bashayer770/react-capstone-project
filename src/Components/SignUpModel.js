9643a9a569ae6e925afe1085949ee748b44ecccb
Bashayer770 <126584700+Bashayer770@users.noreply.github.com>
2023-04-03 06:39:01 +0300

    sign up done

--- src/Components/SignUpModel.js
+++ src/Components/SignUpModel.js
@@ -1,5 +1,10 @@
 import { Button, InputGroup, FormControl, Modal } from "react-bootstrap";
 import { useState } from "react";
+import slogo from "../Assests/slogo.png";
+import UserSvg from "../svg/UserSvg";
+import LockSvg from "../svg/LockSvg";
+import worldmapimg from "../Assests/worldmapimg.png";
+import MailSvg from "../svg/MailSvg";
 
 function SignupModel() {
   const [show, setShow] = useState(false);
@@ -17,38 +22,64 @@ function SignupModel() {
   };
 
   return (
-    <>
-      <Button variant="outline-light" onClick={handleShow}>
-        SIGN UP
-      </Button>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Body>
-          <form onSubmit={handleSubmit}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Username</InputGroup.Text>
-              <FormControl
-                placeholder="Your username"
-                name="username"
-                type="text"
-                onChange={handleChange}
-              />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>Password</InputGroup.Text>
-              <FormControl
-                name="password"
-                type="password"
-                onChange={handleChange}
-                placeholder="Password"
-              />
-            </InputGroup>
-            <Button variant="outline-dark" type="submit">
-              SIGH UP
-            </Button>
-          </form>
-        </Modal.Body>
-      </Modal>
-    </>
+    <div className="backGround_">
+      <div className="motherContainer">
+        <div className="Logo_">
+          <img src={slogo} width="85" />
+          <div className="theh5">
+            <div>SIGHT</div>
+            <div className="Bewhite">S E E R</div>
+          </div>
+        </div>
+        <div className="imageTextContainer">
+          <img className="worldimage" src={worldmapimg} width="100%" />
+          <h6 className="theh6">SIGN UP</h6>
+
+          {/* <Button variant="outline-light mx-3" onClick={handleShow}> */}
+          {/* LOGIN */}
+          {/* </Button> */}
+
+          <Modal.Body>
+            <form onSubmit={handleSubmit}>
+              <InputGroup className="EmailInput">
+                <InputGroup.Text></InputGroup.Text>
+                <MailSvg />
+                <FormControl
+                  name="Your Email address"
+                  type="text"
+                  onChange={handleChange}
+                  placeholder="email"
+                />
+              </InputGroup>
+              <InputGroup className="usernameSignUpInput">
+                <InputGroup.Text></InputGroup.Text>
+                <UserSvg />
+                <FormControl
+                  placeholder=" username"
+                  name="username"
+                  type="text"
+                  onChange={handleChange}
+                />
+              </InputGroup>
+              <InputGroup className="passwordSignUpInput">
+                <InputGroup.Text></InputGroup.Text>
+                <LockSvg />
+                <FormControl
+                  name="password"
+                  type="password"
+                  onChange={handleChange}
+                  placeholder="Password"
+                />
+              </InputGroup>
+
+              <Button className="Button_" variant="outline-dark" type="submit">
+                SIGN UP
+              </Button>
+            </form>
+          </Modal.Body>
+        </div>
+      </div>
+    </div>
   );
 }
 
